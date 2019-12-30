ee75021ffdcac77511a7d118bdbf3158d1480710
DataNick <DataNick@users.noreply.github.com>
2019-12-30 10:43:37 -0500

    Update javascript files

---
+++ config/initializers/cognito.rb
@@ -0,0 +1,10 @@
+# require 'cognito_jwt_keys'
+# require 'cognito_urls'
+#
+# CognitoUrls.init('','us-east-2')
+# CognitoJwtKeysProvider.init('')
+
+require 'cognito_jwt_keys'
+require 'cognito_urls'
+CognitoUrls.init('8rn44c','us-east-2')
+CognitoJwtKeysProvider.init('us-east-2_jWqAkr2hZ')
